b8e245fb7f30eb175c9d05c96f1ac7c9716e19a7
Mohamed Osman <smartbot616@gmail.com>
2026-03-13 11:14:11 +0100

    Hero component little update!

--- src/Components/Landing/Hero.jsx
+++ src/Components/Landing/Hero.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Hero() {
+export default function Hero({ isLoggedIn }) {
   return (
     <section className=" flex space-y-2.5">
       <div className=" relative w-102.5 h-81 hero_img">
@@ -16,13 +16,14 @@ export default function Hero() {
         >
           classes
         </Link>
-        <Link
-          type="button"
-          href="/Login"
-          className="absolute top-3/4 left-6 bg-Uranium text-Black w-24.5 h-12  rounded-3xl transition duration-300 translate-x-30  flex items-center justify-center uppercase"
-        >
-          login
-        </Link>
+      {!isLoggedIn && (
+    <Link
+        href="/Login"
+        className="absolute top-3/4 left-6 bg-Uranium text-Black w-24.5 h-12 rounded-3xl transition duration-300 translate-x-30 flex items-center justify-center uppercase"
+    >
+        login
+    </Link>
+)}
       </div>
     </section>
   );
--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -1,3 +1,4 @@
+import { cookies } from "next/headers";
 import ContactLanding from "@/Components/contactform";
 import Hero from "@/Components/Landing/Hero";
 import NewsList from "@/Components/Landing/News";
@@ -5,18 +6,22 @@ import Newsletter from "@/Components/Landing/newsletter";
 import TestimonialSlider from "@/Components/Landing/testimonials";
 
 export const metadata = {
-  title: "Landing page",
-  description: "Velkommen til vores landing page",
+    title: "Landing page",
+    description: "Velkommen til vores landing page",
 };
-export default function Home() {
-  return (
-    <>
-      <Hero />
-      <NewsList />
-      <hr className=" text-black w-10 h-0.5 mx-auto " />
-      <Newsletter />
-      <TestimonialSlider />
-      <ContactLanding />
-    </>
-  );
+
+export default async function Home() {
+    const cookieStore = await cookies();
+    const isLoggedIn = !!cookieStore.get("fitness-accessToken");
+
+    return (
+        <>
+            <Hero isLoggedIn={isLoggedIn} />
+            <NewsList />
+             <hr className=" text-black w-10 h-0.5 mx-auto " />
+            <Newsletter />
+            <TestimonialSlider />
+            <ContactLanding />
+        </>
+    );
 }
